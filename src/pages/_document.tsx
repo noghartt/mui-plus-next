5ec4624a4512612d12eb67adc4b4356cd43f75b2
Guilherme Ananias <hi@noghartt.dev>
2023-03-10 08:56:50 -0300

    mui next ssg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,55 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance';
 
-export default function Document() {
+import createEmotionCache from '../createEmotionCache';
+import { roboto } from '../theme';
+
+export default function MyDocument({ emotionStyleTags }) {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className={roboto.className}>
+      <Head>
+        <meta name="emotion-insertion-point" content="" />
+        {emotionStyleTags}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const originalRenderPage = ctx.renderPage;
+
+  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents Emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
